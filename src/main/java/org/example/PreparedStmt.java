0b0b9ab22d48af4f8d4863373f444de7d2cf2533
kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2025-12-06 15:13:18 +0530

    PrepareStatement implementation to avoid SQL Injection threat

---
+++ src/main/java/org/example/PreparedStmt.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.sql.*;
+import java.sql.SQLException;
+
+public class PreparedStmt {
+    public static void main(String args[]) throws Exception {
+
+        String url="jdbc:postgresql://localhost:5432/JavaDB";
+        String uname="postgres";
+        String pw ="Jeetu@9712";
+        String sql = "INSERT INTO students VALUES (?,?)";
+        int id = 4;
+        String name = "Aklavya";
+        Connection con = DriverManager.getConnection(url,uname,pw);
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1,id);
+        ps.setString(2,name);
+        ps.execute();
+        con.close();
+    }
+}
